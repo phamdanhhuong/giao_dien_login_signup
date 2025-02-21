1511283c13389d454058d44193833bb1d59ce9c2
phamdanhhuong <danhhuong246@gmail.com>
2025-02-21 13:40:41 +0700

    Thêm trang đăng ký

--- app/src/main/java/com/example/giao_dien_login_signup/MainActivity.java
+++ app/src/main/java/com/example/giao_dien_login_signup/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.giao_dien_login_signup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TextView register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
